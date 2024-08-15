17c77f428cddf35bbe4bc63d3ddf123b8325434f
Efy <35348263+Efnilite@users.noreply.github.com>
2024-08-15 02:15:56 +0200

    Add trailing empty string ignoring and tests to ExprJoinSplit (#6930)
    * init commit innit
    
    * minor touch ups
    
    * oops
    
    * fix annotation
    
    * oopsie
    
    * fixes
    
    * removed unused import
    
    * update annotation
    
    * added text
    
    ---------
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/expressions/ExprJoinSplit.java
+++ src/main/java/ch/njol/skript/expressions/ExprJoinSplit.java
@@ -1,30 +1,7 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.expressions;
 
-import java.util.regex.Pattern;
-
-import ch.njol.skript.SkriptConfig;
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
-
 import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptConfig;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
@@ -35,43 +12,48 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import ch.njol.util.StringUtils;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
 
-/**
- * @author Peter Güttinger
- */
+import java.util.regex.Pattern;
+
 @Name("Join & Split")
 @Description("Joins several texts with a common delimiter (e.g. \", \"), or splits a text into multiple texts at a given delimiter.")
 @Examples({
-		"message \"Online players: %join all players with \"\" | \"\"%\" # %all players% would use the default \"x, y, and z\"",
-		"set {_s::*} to the string argument split at \",\""
+	"message \"Online players: %join all players' names with \"\" | \"\"%\" # %all players% would use the default \"x, y, and z\"",
+	"set {_s::*} to the string argument split at \",\""
 })
-@Since("2.1, 2.5.2 (regex support), 2.7 (case sensitivity)")
+@Since("2.1, 2.5.2 (regex support), 2.7 (case sensitivity), INSERT VERSION (without trailing string)")
 public class ExprJoinSplit extends SimpleExpression<String> {
 
 	static {
 		Skript.registerExpression(ExprJoinSplit.class, String.class, ExpressionType.COMBINED,
 			"(concat[enate]|join) %strings% [(with|using|by) [[the] delimiter] %-string%]",
-			"split %string% (at|using|by) [[the] delimiter] %string% [case:with case sensitivity]",
-			"%string% split (at|using|by) [[the] delimiter] %string% [case:with case sensitivity]",
-			"regex split %string% (at|using|by) [[the] delimiter] %string%",
-			"regex %string% split (at|using|by) [[the] delimiter] %string%");
+			"split %string% (at|using|by) [[the] delimiter] %string% [case:with case sensitivity] [trailing:without [the] trailing [empty] (string|text)]",
+			"%string% split (at|using|by) [[the] delimiter] %string% [case:with case sensitivity] [trailing:without [the] trailing [empty] (string|text)]",
+			"regex split %string% (at|using|by) [[the] delimiter] %string% [trailing:without [the] trailing [empty] (string|text)]",
+			"regex %string% split (at|using|by) [[the] delimiter] %string% [trailing:without [the] trailing [empty] (string|text)]");
 	}
 
 	private boolean join;
 	private boolean regex;
 	private boolean caseSensitivity;
+	private boolean removeTrailing;
 
-	@SuppressWarnings("null")
+	@UnknownNullability
 	private Expression<String> strings;
-	@Nullable
+	@UnknownNullability
 	private Expression<String> delimiter;
 
 	@Override
-	@SuppressWarnings({"unchecked", "null"})
+	@SuppressWarnings("unchecked")
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		join = matchedPattern == 0;
 		regex = matchedPattern >= 3;
 		caseSensitivity = SkriptConfig.caseSensitive.value() || parseResult.hasTag("case");
+		removeTrailing = parseResult.hasTag("trailing");
+
 		strings = (Expression<String>) exprs[0];
 		delimiter = (Expression<String>) exprs[1];
 		return true;
@@ -82,12 +64,18 @@ public class ExprJoinSplit extends SimpleExpression<String> {
 	protected String[] get(Event event) {
 		String[] strings = this.strings.getArray(event);
 		String delimiter = this.delimiter != null ? this.delimiter.getSingle(event) : "";
+
 		if (strings.length == 0 || delimiter == null)
 			return new String[0];
+
 		if (join) {
-			return new String[] {StringUtils.join(strings, delimiter)};
+			return new String[]{StringUtils.join(strings, delimiter)};
 		} else {
-			return strings[0].split(regex ? delimiter : (caseSensitivity ? "" : "(?i)") + Pattern.quote(delimiter), -1);
+			if (!regex) {
+				delimiter = (caseSensitivity ? "" : "(?i)") + Pattern.quote(delimiter);
+			}
+
+			return strings[0].split(delimiter, removeTrailing ? 0 : -1);
 		}
 	}
 
@@ -104,9 +92,13 @@ public class ExprJoinSplit extends SimpleExpression<String> {
 	@Override
 	public String toString(@Nullable Event event, boolean debug) {
 		if (join)
-			return "join " + strings.toString(event, debug) + (delimiter != null ? " with " + delimiter.toString(event, debug) : "");
-		return (regex ? "regex " : "") + "split " + strings.toString(event, debug) + (delimiter != null ? " at " + delimiter.toString(event, debug) : "")
-			+ (regex ? "" : "(case sensitive: " + caseSensitivity + ")");
+			return "join " + strings.toString(event, debug) +
+				(delimiter != null ? " with " + delimiter.toString(event, debug) : "");
+		return (regex ? "regex " : "") +
+			"split " + strings.toString(event, debug) +
+			(delimiter != null ? " at " + delimiter.toString(event, debug) : "") +
+			(regex ? "" : "(case sensitive: " + caseSensitivity + ")") +
+			(removeTrailing ? " without trailing string" : "");
 	}
 
 }
