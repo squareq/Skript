09b78b0de251e16eda1c4374b78ea65fc9a11f7b
Efnilite <35348263+Efnilite@users.noreply.github.com>
2024-10-13 16:09:00 +0200

    Avoid sending update message on join when testing (#7106)
    * init commit
    
    * invert if
    
    ---------
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/Skript.java
+++ src/main/java/ch/njol/skript/Skript.java
@@ -838,32 +838,9 @@ public final class Skript extends JavaPlugin implements Listener {
 			}
 		});
 
-		Bukkit.getPluginManager().registerEvents(new Listener() {
-			@EventHandler
-			public void onJoin(final PlayerJoinEvent e) {
-				if (e.getPlayer().hasPermission("skript.admin")) {
-					new Task(Skript.this, 0) {
-						@Override
-						public void run() {
-							Player p = e.getPlayer();
-							SkriptUpdater updater = getUpdater();
-							if (updater == null)
-								return;
-
-							// Don't actually check for updates to avoid breaking Github rate limit
-							if (updater.getReleaseStatus() == ReleaseStatus.OUTDATED) {
-								// Last check indicated that an update is available
-								UpdateManifest update = updater.getUpdateManifest();
-								assert update != null; // Because we just checked that one is available
-								Skript.info(p, "" + SkriptUpdater.m_update_available.toString(update.id, Skript.getVersion()));
-								p.spigot().sendMessage(BungeeConverter.convert(ChatMessages.parseToArray(
-										"Download it at: <aqua><u><link:" + update.downloadUrl + ">" + update.downloadUrl)));
-							}
-						}
-					};
-				}
-			}
-		}, this);
+		if (!TestMode.ENABLED) {
+			Bukkit.getPluginManager().registerEvents(new JoinUpdateNotificationListener(), this);
+		}
 
 		// Send a warning to console when the plugin is reloaded
 		Bukkit.getPluginManager().registerEvents(new Listener() {
@@ -887,6 +864,36 @@ public final class Skript extends JavaPlugin implements Listener {
 		// Tell Timings that we are here!
 		SkriptTimings.setSkript(this);
 	}
+
+	private class JoinUpdateNotificationListener implements Listener {
+		@EventHandler
+		public void onJoin(PlayerJoinEvent event) {
+			if (!event.getPlayer().hasPermission("skript.admin"))
+				return;
+
+			new Task(Skript.this, 0) {
+				@Override
+				public void run() {
+					Player player = event.getPlayer();
+					SkriptUpdater updater = getUpdater();
+
+					// Don't actually check for updates to avoid breaking GitHub rate limit
+					if (updater == null || updater.getReleaseStatus() != ReleaseStatus.OUTDATED)
+						return;
+
+					// Last check indicated that an update is available
+					UpdateManifest update = updater.getUpdateManifest();
+
+					if (update == null)
+						return;
+
+					Skript.info(player, SkriptUpdater.m_update_available.toString(update.id, Skript.getVersion()));
+					player.spigot().sendMessage(BungeeConverter.convert(ChatMessages.parseToArray(
+						"Download it at: <aqua><u><link:" + update.downloadUrl + ">" + update.downloadUrl)));
+				}
+			};
+		}
+  }
 
 	private void runTest(Class<?> clazz, AtomicLong shutdownDelay, AtomicLong tests,
 						 AtomicLong milliseconds, AtomicLong ignored, AtomicLong fails)
