480cecd2f7b6c69f0e8190f67d7d6270659d6632
sovdee <10354869+sovdeeth@users.noreply.github.com>
2024-10-15 13:23:21 -0400

    remove eclipse annotations in display rotation packageinfo (#7145)
    * remove eclipse annotations in display rotation packageinfo
    
    * Update build.gradle
    
    * Update build.gradle
    
    ---------
    
    Co-authored-by: Moderocky <admin@moderocky.com>

--- src/main/java/org/skriptlang/skript/bukkit/misc/rotation/package-info.java
+++ src/main/java/org/skriptlang/skript/bukkit/misc/rotation/package-info.java
@@ -1,5 +1,1 @@
-@NonNullByDefault({DefaultLocation.PARAMETER, DefaultLocation.RETURN_TYPE})
 package org.skriptlang.skript.bukkit.misc.rotation;
-
-import org.eclipse.jdt.annotation.DefaultLocation;
-import org.eclipse.jdt.annotation.NonNullByDefault;
