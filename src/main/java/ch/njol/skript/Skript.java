6ba6706906658ec312714126167f73e584fbe636
_tud <98935832+UnderscoreTud@users.noreply.github.com>
2024-11-23 07:53:46 +0300

    Input API (#7190)
    * Input API
    
    * Add `Minecraft 1.21.3+` to the requirements of all the added elements
    
    * Requested changes
    
    * Add a way to check whether a key is toggled, pressed or released
    
    * Add note to event description
    
    * Requested Changes
    
    * Revert unchanged files
    
    * Remove toString method of InputKey
    
    * Requested changes
    
    * Replace StringBuilder with SyntaxStringBuilder
    
    * Fix version checks
    
    * Fix tests failing on unsupported versions
    
    * Fix tests failing on unsupported versions
    
    * replace spaces with tabs
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * Update src/main/java/org/skriptlang/skript/bukkit/input/InputModule.java
    
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>
    
    * update pattern
    
    * Fix event example
    
    ---------
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>
    Co-authored-by: Efnilite <35348263+Efnilite@users.noreply.github.com>
    Co-authored-by: Patrick Miller <apickledwalrus@gmail.com>

--- src/main/java/ch/njol/skript/Skript.java
+++ src/main/java/ch/njol/skript/Skript.java
@@ -96,6 +96,7 @@ import org.junit.runner.notification.Failure;
 import org.skriptlang.skript.bukkit.SkriptMetrics;
 import org.skriptlang.skript.bukkit.breeding.BreedingModule;
 import org.skriptlang.skript.bukkit.displays.DisplayModule;
+import org.skriptlang.skript.bukkit.input.InputModule;
 import org.skriptlang.skript.lang.comparator.Comparator;
 import org.skriptlang.skript.lang.comparator.Comparators;
 import org.skriptlang.skript.lang.converter.Converter;
@@ -555,8 +556,9 @@ public final class Skript extends JavaPlugin implements Listener {
 				"conditions", "effects", "events", "expressions", "entity", "sections", "structures");
 			getAddonInstance().loadClasses("org.skriptlang.skript.bukkit", "misc");
 			// todo: become proper module once registry api is merged
-			DisplayModule.load();
 			BreedingModule.load();
+			DisplayModule.load();
+			InputModule.load();
 		} catch (final Exception e) {
 			exception(e, "Could not load required .class files: " + e.getLocalizedMessage());
 			setEnabled(false);
