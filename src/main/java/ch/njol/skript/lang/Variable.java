4415c23772e325f2b79a75d098f4c3012ea72c42
Patrick Miller <apickledwalrus@gmail.com>
2024-12-14 22:24:45 -0500

    Fix CIP implementation for Variable (#7241)
    Implement other CIP method on Variable
    
    Co-authored-by: sovdee <10354869+sovdeeth@users.noreply.github.com>

--- src/main/java/ch/njol/skript/lang/Variable.java
+++ src/main/java/ch/njol/skript/lang/Variable.java
@@ -615,6 +615,15 @@ public class Variable<T> implements Expression<T> {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @param getAll This has no effect for a Variable, as {@link #getArray(Event)} is the same as {@link #getAll(Event)}.
+	 */
+	@Override
+	public <R> void changeInPlace(Event event, Function<T, R> changeFunction, boolean getAll) {
+		changeInPlace(event, changeFunction);
+	}
+
 	@Override
 	public <R> void changeInPlace(Event event, Function<T, R> changeFunction) {
 		if (!list) {
