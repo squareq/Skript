264baaa85ef5b83aea8ec46d7f688ab42544153f
sovdee <10354869+sovdeeth@users.noreply.github.com>
2025-01-03 06:57:39 -0500

    Increase test delay time. (#7371)
    apparently 20 ticks wasn't enough
    
    Co-authored-by: Moderocky <admin@moderocky.com>

--- src/main/java/ch/njol/skript/Skript.java
+++ src/main/java/ch/njol/skript/Skript.java
@@ -790,7 +790,7 @@ public final class Skript extends JavaPlugin implements Listener {
 								Bukkit.getServer().shutdown();
 							}, shutdownDelay.get());
 						});
-					}, 20); // 20 ticks is necessary on 1.19.4 to allow the server time to properly load/start ticking, for some reason
+					}, 50); // 50 ticks is necessary on 1.19.4/1.20.4 to allow the server time to properly load/start ticking, for some reason
 				}
 
 				Skript.metrics = new Metrics(Skript.getInstance(), 722); // 722 is our bStats plugin ID
