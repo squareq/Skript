86efdcd68e995681e1e8ffb6ab982b6eb8dc8ad2
SirSmurfy2 <82696841+TheAbsolutionism@users.noreply.github.com>
2025-03-20 09:36:04 -0400

    Ghast Support (#7597)
    * Initial Commit
    
    * Requested Changes
    
    * Float Helper Method
    
    ---------
    
    Co-authored-by: Efnilite <35348263+Efnilite@users.noreply.github.com>

--- src/main/java/ch/njol/skript/expressions/ExprExplosiveYield.java
+++ src/main/java/ch/njol/skript/expressions/ExprExplosiveYield.java
@@ -1,120 +1,98 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.doc.*;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.util.Math2;
+import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Explosive;
+import org.bukkit.entity.Ghast;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-import ch.njol.skript.Skript;
-import ch.njol.skript.classes.Changer.ChangeMode;
-import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Examples;
-import ch.njol.skript.doc.Name;
-import ch.njol.skript.doc.RequiredPlugins;
-import ch.njol.skript.doc.Since;
-import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import ch.njol.util.coll.CollectionUtils;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Name("Explosive Yield")
-@Description({"The yield of an explosive (creeper, primed tnt, fireball, etc.). This is how big of an explosion is caused by the entity.",
-				"Read <a href='https://minecraft.wiki/w/Explosion'>this wiki page</a> for more information"})
-@Examples({"on spawn of a creeper:",
-			"\tset the explosive yield of the event-entity to 10"})
-@RequiredPlugins("Minecraft 1.12 or newer for creepers")
-@Since("2.5")
+@Description({
+	"The yield of an explosive (creeper, ghast, primed tnt, fireball, etc.). This is how big of an explosion is caused by the entity.",
+	"Read <a href='https://minecraft.wiki/w/Explosion'>this wiki page</a> for more information.",
+	"The yield of ghasts can only be set to between 0 and 127."
+})
+@Examples({
+	"on spawn of a creeper:",
+		"\tset the explosive yield of the event-entity to 10"
+})
+@RequiredPlugins("Paper (ghasts)")
+@Since("2.5, INSERT VERSION (ghasts)")
 public class ExprExplosiveYield extends SimplePropertyExpression<Entity, Number> {
 
+	private static final boolean SUPPORTS_GHASTS = Skript.methodExists(Ghast.class, "getExplosionPower");
+
 	static {
-		register(ExprExplosiveYield.class, Number.class, "explosive (yield|radius|size)", "entities");
-	}
-
-	private final static boolean CREEPER_USABLE = Skript.methodExists(Creeper.class, "getExplosionRadius");
-
-	@Override
-	public Number convert(Entity e) {
-		if (e instanceof Explosive)
-			return ((Explosive) e).getYield();
-		if (CREEPER_USABLE && e instanceof Creeper)
-			return ((Creeper) e).getExplosionRadius();
-		return 0;
+		register(ExprExplosiveYield.class, Number.class, "explosive (yield|radius|size|power)", "entities");
 	}
 
 	@Override
-	@Nullable
-	public Class<?>[] acceptChange(final ChangeMode mode) {
-		switch (mode) {
-			case SET:
-			case ADD:
-			case REMOVE:
-			case DELETE:
-				return CollectionUtils.array(Number.class);
-			default:
-				return null;
+	public @Nullable Number convert(Entity entity) {
+		if (entity instanceof Explosive explosive) {
+			return explosive.getYield();
+		} else if (entity instanceof Creeper creeper) {
+			return creeper.getExplosionRadius();
+		} else if (SUPPORTS_GHASTS && entity instanceof Ghast ghast) {
+			return ghast.getExplosionPower();
 		}
+		return null;
 	}
 
 	@Override
-	public void change(final Event event, final @Nullable Object[] delta, final ChangeMode mode) {
-		Number change = delta != null ? (Number) delta[0] : 0;
+	public Class<?> @Nullable [] acceptChange(ChangeMode mode) {
+		return switch (mode) {
+			case SET, DELETE, ADD, REMOVE -> CollectionUtils.array(Number.class);
+			default -> null;
+		};
+	}
+
+	@Override
+	public void change(Event event, Object @Nullable [] delta, ChangeMode mode) {
+		Number number = delta != null ? (Number) delta[0] : 0;
+		float floatValue = Math2.fit(0, number.floatValue(), Float.MAX_VALUE);
+		int intValue = Math2.fit(0, number.intValue(), Integer.MAX_VALUE);
+
 		for (Entity entity : getExpr().getArray(event)) {
-			if (entity instanceof Explosive) {
-				Explosive e = (Explosive) entity;
-				float f = change.floatValue();
-				if (f < 0) // Negative values will throw an error.
-					return;
-				switch (mode) {
-					case SET:
-						e.setYield(f);
-						break;
-					case ADD:
-						float add = e.getYield() + f;
-						if (add < 0)
-							return;
-						e.setYield(add);
-						break;
-					case REMOVE:
-						float subtract = e.getYield() - f;
-						if (subtract < 0)
-							return;
-						e.setYield(subtract);
-						break;	
-					case DELETE:
-						e.setYield(0);
-						break;
-					default:
-						assert false;
-				}
-			} else if (CREEPER_USABLE && entity instanceof Creeper) {
-				Creeper c = (Creeper) entity;
-				int i = change.intValue();
-				if (i < 0) // Negative values will throw an error.
-					return;
-				switch (mode) {
-					case SET:
-						c.setExplosionRadius(i);
-						break;
-					case ADD:
-						int add = c.getExplosionRadius() + i;
-						if (add < 0)
-							return;
-						c.setExplosionRadius(add);
-						break;
-					case REMOVE:
-						int subtract = c.getExplosionRadius() - i;
-						if (subtract < 0)
-							return;
-						c.setExplosionRadius(subtract);
-						break;	
-					case DELETE:
-						c.setExplosionRadius(0);
-						break;
-					case REMOVE_ALL:
-					case RESET:
-						assert false;
-				}
+			if (entity instanceof Explosive explosive) {
+				changeExplosion(mode, floatValue, explosive::getYield, explosive::setYield, Float.MAX_VALUE);
+			} else if (entity instanceof Creeper creeper) {
+				changeExplosion(mode, intValue, creeper::getExplosionRadius, creeper::setExplosionRadius, Integer.MAX_VALUE);
+			} else if (SUPPORTS_GHASTS && entity instanceof Ghast ghast) {
+				changeExplosion(mode, intValue, ghast::getExplosionPower, ghast::setExplosionPower, 127);
 			}
 		}
+	}
+
+	private void changeExplosion(ChangeMode mode, float value, Supplier<Float> getter, Consumer<Float> setter, float max) {
+		setter.accept(Math2.fit(0F,
+			switch (mode) {
+				case SET, DELETE -> value;
+				case ADD -> getter.get() + value;
+				case REMOVE -> getter.get() - value;
+				default -> throw new IllegalArgumentException("Unexpected mode: " + mode);
+			},
+			max));
+	}
+
+	private void changeExplosion(ChangeMode mode, int value, Supplier<Integer> getter, Consumer<Integer> setter, int max) {
+		setter.accept(Math2.fit(0,
+				switch (mode) {
+					case SET, DELETE -> value;
+					case ADD -> getter.get() + value;
+					case REMOVE -> getter.get() - value;
+					default -> throw new IllegalArgumentException("Unexpected mode: " + mode);
+				},
+			max));
 	}
 
 	@Override
